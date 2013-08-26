cbd7a30ee1adbc98f5f5e16ca57d1ef04125f614
Vincent Grato <vgrato@gmail.com>
2013-08-25 18:34:42 -0700

    Rotate tiles right and left

--- src/pentago/core.clj
+++ src/pentago/core.clj
@@ -3,24 +3,32 @@
 
 (defprotocol Flatten
   "Can be converted from internal moveable structure to vector"
-  (to_a [_]))
-
-(defprotocol Rotate
-  "Can be converted from internal moveable structure to vector"
-  (rotate_tile_right [_])
-  (left [_]))
+  (to_a [_])
+  )
 
 (defn rotate_list_right [s]
-  (cons (last s) (drop-last s)))
+  (concat (list (last s)) (butlast s))
+  )
+
+(defn rotate_list_left [s]
+  (concat (rest s) (list (first s)))
+  )
 
 (defrecord Tile [ring anchor]
   Flatten
     (to_a [_] [(into [] (take 3 ring))
              [(nth ring 7) anchor (nth ring 3)]
              [(nth ring 6) (nth ring 5) (nth ring 4)]])
-  Rotate
-    (rotate_tile_right [_] (Tile. (rotate_list_right (rotate_list_right ring) anchor)))
-    (left [_] (str "rotating left"))
+  )
+
+(defn rotate_tile_right [s]
+  (Tile. (rotate_list_right (rotate_list_right (:ring s)))
+         (:anchor s))
+  )
+
+(defn rotate_tile_left [s]
+  (Tile. (rotate_list_left (rotate_list_left (:ring s)))
+         (:anchor s))
   )
 
 (defn blank_tile [] (Tile. `(nil nil nil nil nil nil nil nil) nil))
@@ -29,4 +37,6 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (rotate_tile_right (first args)))
+  (rotate_tile_right (first args))
+  )
+
